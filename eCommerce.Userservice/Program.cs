67cde2e5c5abe18ac908c4cead96156a34b8474f
Shubham Todkar <todkarshubham11@gmail.com>
2025-12-25 17:48:35 +0530

    Added swagger

--- eCommerce.Userservice/Program.cs
+++ eCommerce.Userservice/Program.cs
@@ -12,13 +12,30 @@ builder.Services.AddCore();
 builder.Services.AddControllers().AddJsonOptions(opt => opt.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter()));
 builder.Services.AddAutoMapper(typeof(ApplicationUserMappingProfile).Assembly);
 builder.Services.AddValidatorsFromAssemblyContaining<LoginRequestValidators>();
+
+//Add api explorer
+builder.Services.AddEndpointsApiExplorer();
+
+//Add swagger generation
+builder.Services.AddSwaggerGen();
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll",
+        builder =>
+        {
+            builder.AllowAnyOrigin()
+                   .AllowAnyMethod()
+                   .AllowAnyHeader();
+        });
+});
 var app = builder.Build();
 app.UseExceptionHandlingMiddleware();
 
 
 app.UseRouting();
+app.UseSwagger();
+app.UseSwaggerUI();
 app.UseHttpsRedirection();
-app.UseCors("AllowAll");
 app.MapControllers();
 app.UseAuthentication();
 app.UseAuthorization();
